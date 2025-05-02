e1b86caf25d7c6fb35d6d5aaa3593dfe2164009d
marcoscondejr <marcos14conde@hotmail.com>
2025-05-02 00:08:40 -0300

    refactor: mudando arquivo de conexão com o banco utilizando o sequelize

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,16 +1,16 @@
+const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
-const { underscoredIf } = require('sequelize/lib/utils');
 
 dotenv.config();
 
-module.exports = {
-    dialect: process.env.DB_DIALECT,
-    username: process.env.DB_USER,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
     define: {
-        timestamps: true,
-        underscored: true
+      timestamps: true,
     }
-};
+  }
+);
+
+module.exports = sequelize;
