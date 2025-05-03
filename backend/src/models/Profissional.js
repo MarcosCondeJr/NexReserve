539bf32c9b3ea48f9877a242e71d5268809d5875
marcoscondejr <marcos14conde@hotmail.com>
2025-05-03 00:17:39 -0300

    feat: criando a classe de profissional

---
+++ backend/src/models/Profissional.js
@@ -0,0 +1,30 @@
+const { DataTypes, Mode, Model } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Profissional extends Model {}
+
+Profissional.init({
+    id_profissional: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    nm_profissional: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    email_profissional: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        unique: true
+    },
+    telefone_profissional: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    }}, {
+        sequelize,
+        modelName: 'Profissional',
+        tableName: 'profissionais',
+        timestamps: true
+})
