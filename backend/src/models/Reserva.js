f7bc2f54a3a8676185b1821a2358edffb8d81dac
marcoscondejr <marcos14conde@hotmail.com>
2025-05-03 23:06:22 -0300

    feat: criando a model de Reserva e as associações

---
+++ backend/src/models/Reserva.js
@@ -0,0 +1,43 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/database');
+
+class Reserva extends Model {}
+
+Reserva.init({
+    id_reserva: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+    },
+    id_usuario: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Usuario',
+            key: 'id_usuario'
+        }
+    },
+    id_servico: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Servico',
+            key: 'id_servico'
+        }
+    },
+    data_reserva: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    status_reserva: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }}, {
+        sequelize,
+        modelName: 'Reserva',
+        tableName: 'reservas',
+        timestamps: true
+});
+
+module.exports = Reserva;
--- backend/src/models/associations.js
+++ backend/src/models/associations.js
@@ -1,5 +1,7 @@
 const Profissional = require('./Profissional');
 const Servico = require('./Servico');
+const Reserva = require('./Reserva');
+const Usuario = require('./Usuario');
 
 Profissional.hasMany(Servico, {
   foreignKey: 'id_profissional',
@@ -8,7 +10,17 @@ Profissional.hasMany(Servico, {
 
 Servico.belongsTo(Profissional, {
   foreignKey: 'id_profissional',
-  as: 'profissionais'
+  as: 'profissional'
 });
 
-module.exports = { Profissional, Servico };
+Reserva.belongsTo(Servico, {
+    foreignKey: 'id_servico',
+    as: 'servico'
+});
+
+Reserva.belongsTo(Usuario, {
+    foreignKey: 'id_usuario',
+    as: 'usuario'
+});
+
+module.exports = { Profissional, Servico, Reserva };
